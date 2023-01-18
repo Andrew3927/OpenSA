a3869ba0a5a1eb87f90a7efabe5e60d31434bcb1
Andrew3927 <1260805505c@users.noreply.github.com>
2023-01-18 16:10:10 +0800

    fix bug & update

--- OpenSA/Plot/SpectrumPlot.py
+++ OpenSA/Plot/SpectrumPlot.py
@@ -13,7 +13,6 @@ def plotspc(title):
     Nirdata1 = np.concatenate((Cdata1, Vdata1))
     Nirdata = np.concatenate((Nirdata1, Tdata1))
     l = np.loadtxt(open('./Data/Rgs/l.csv', 'rb'), dtype=np.float64, delimiter=',', skiprows=0)
-    l = np.loadtxt(open('l.csv', 'rb'), dtype=np.float64, delimiter=',', skiprows=0)
     data = Nirdata[:, :-3]
     label = Nirdata[:, -3:-1]
     l = l[1:, ]
